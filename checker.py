940e6497a74e83a96f310354a602cd107217000c
Reaper <klithish60@gmail.com>
2025-07-17 01:12:45 -0700

    Add files via upload

---
+++ checker.py
@@ -0,0 +1,20 @@
+import re
+
+def check_password_strength(password):
+    score = 0
+    if len(password) >= 8:
+        score += 1
+    if re.search(r"[A-Z]", password):
+        score += 1
+    if re.search(r"[a-z]", password):
+        score += 1
+    if re.search(r"[0-9]", password):
+        score += 1
+    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        score += 1
+
+    return score
+
+password = input("Enter a password to check: ")
+strength = check_password_strength(password)
+print(f"Password strength score: {strength}/5")
